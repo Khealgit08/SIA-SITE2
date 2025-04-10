9d455ee00ab98d01203577aa0e7fa51b920853bc
Khealgit08 <castilmichael08@gmail.com>
2025-04-10 21:44:55 +0800

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Usertbl;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Response; 
+
+Class UserController extends Controller {
+
+    private $request;
+
+    use ApiResponser;
+
+    public function __construct(Request $request){
+        $this->request = $request;
+    }
+
+    public function getUsers(){
+        $users = Usertbl::all();
+        return response()->json($users, 200);
+    }
+
+    /**
+    * Return the list of users
+    * @return Illuminate\Http\Response
+    */
+    public function index(){
+        $users = Usertbl::all();
+        return $this->successResponse($users);
+    }
+
+    public function add(Request $request ){
+        $rules = [
+            'username' => 'required|max:20',
+            'password' => 'required|max:20',
+            'gender' => 'required|in:Male,Female',
+        ];
+        
+        $this->validate($request,$rules);
+        $user = Usertbl::create($request->all());
+        return $this->successResponse($user,Response::HTTP_CREATED);
+    }
+
+    /**
+    * Obtains and show one user
+    * @return Illuminate\Http\Response
+    */
+    public function show($id){
+        $user = Usertbl::findOrFail($id);
+        return $this->successResponse($user);
+    // old code
+    /*
+    $user = User::where('userid', $id)->first();
+    if($user){
+    return $this->successResponse($user);
+    }
+    {
+    return $this->errorResponse('User ID Does Not Exists',
+    Response::HTTP_NOT_FOUND);
+    }
+    */
+    }
+
+    /**
+    * Update an existing author
+    * @return Illuminate\Http\Response
+    */
+    public function update(Request $request,$id){
+        $rules = [
+            'username' => 'max:20',
+            'password' => 'max:20',
+            'gender' => 'in:Male,Female',
+        ];
+
+        $this->validate($request, $rules);
+        $user = Usertbl::findOrFail($id);
+        $user->fill($request->all());
+
+        // if no changes happen
+        if ($user->isClean()) {
+        return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $user->save();
+        return $this->successResponse($user);
+        // old code
+        /*
+        $this->validate($request, $rules);
+        //$user = User::findOrFail($id);
+        $user = User::where('userid', $id)->first();
+        if($user){
+        $user->fill($request->all());
+        // if no changes happen
+        if ($user->isClean()) {
+        return $this->errorResponse('At least one value must
+        change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $user->save();
+        return $this->successResponse($user);
+        }
+        {
+        return $this->errorResponse('User ID Does Not Exists',
+        Response::HTTP_NOT_FOUND);
+        }
+        */
+    }
+
+    /**
+    * Remove an existing user
+    * @return Illuminate\Http\Response
+    */
+    public function delete($id){
+        $user = Usertbl::findOrFail($id);
+        $user->delete();
+
+        return $this->successResponse($user);
+        // old code
+        /*
+        $user = User::where('userid', $id)->first();
+        if($user){
+        $user->delete();
+        return $this->successResponse($user);
+        }
+        {
+        return $this->errorResponse('User ID Does Not Exists',
+        Response::HTTP_NOT_FOUND);
+        }
+        */
+    }
+}
---
+++ app/Models/Usertbl.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usertbl extends Model{
+
+    protected $table = 'tblusers';
+
+    // column sa table
+    protected $fillable = [
+        'username', 'password', 'gender',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+}
